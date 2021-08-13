5d3e66ad2a24d5ee8923f8104e2b9c56ce68af4d
Yusuke Yatsuo <yyatsuo@gmail.com>
2021-08-13 16:33:43 +0900

    [update] Use json instead of CSV file
    This commit fix #1

--- bbs.go
+++ bbs.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
 type Comment struct {
-	Name string
-	Date string
-	Cont string
+	Name string `json:"Name"`
+	Date string `json:"Date"`
+	Cont string `json:"Cont"`
 }
 
 type Thread struct {
-	Title    string
-	Id       string
-	Comments []Comment
+	Title    string    `json:"Title"`
+	Id       string    `json:"Id"`
+	Comments []Comment `json:"Comments"`
 }
 
 func check(err error) {
 	// TODO: Gracefully handle errors
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
 
@@ -60,65 +63,70 @@ func threadListHandler(writer http.ResponseWriter, request *http.Request) {
 func threadCreateHandler(writer http.ResponseWriter, request *http.Request) {
 	title := request.FormValue("title")
 	id := uuid.NewString()
+
 	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
-	file, err := os.OpenFile("dat/threads.csv", options, os.FileMode(0600))
+
+	dat, err := json.Marshal(Thread{Title: title, Id: id})
 	check(err)
-	defer file.Close()
-	os.Create("dat/" + id)
-	fmt.Fprintf(file, "%s,%s\n", title, id)
+	err = ioutil.WriteFile("dat/"+id, dat, 0644)
+	check(err)
+
+	threadfile, err := os.OpenFile("dat/threads.csv", options, os.FileMode(0600))
+	check(err)
+	defer threadfile.Close()
+	fmt.Fprintf(threadfile, "%s,%s\n", title, id)
+
 	http.Redirect(writer, request, "/", http.StatusFound)
 }
 
 func threadViewHandler(writer http.ResponseWriter, request *http.Request) {
+
 	id := request.URL.Query().Get("id")
-	title := request.URL.Query().Get("title")
 	datfile := "dat/" + id
 	_, err := os.Stat(datfile)
-	if id == "" || title == "" || err != nil {
-		fmt.Println("Invalid request id:", id, "title:", title)
-		http.Redirect(writer, request, "/", http.StatusFound)
-		return
+	check(err)
+
+	raw, err := ioutil.ReadFile(datfile)
+	check(err)
+	var thread Thread
+	json.Unmarshal(raw, &thread)
+
+	funcMap := template.FuncMap{
+		"crlf2br": func(s string) template.HTML {
+			return template.HTML(strings.Replace(s, "\r\n", "<br>", -1)) 
+		},
 	}
-
-	file, err := os.Open(datfile)
+	html, err := template.New("view.html").Funcs(funcMap).ParseFiles("template/view.html")
 	check(err)
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	var comments []Comment
-
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		check(err)
-		comments = append(comments, Comment{Name: record[0], Date: record[1], Cont: record[2]})
-	}
-
-	html, err := template.ParseFiles("template/view.html")
-	check(err)
-
-	err = html.Execute(writer, Thread{Title: title, Id: id, Comments: comments})
-	check(err)
+	check(html.Execute(writer, thread))
 }
 
 func addCommentHandler(writer http.ResponseWriter, request *http.Request) {
 	id := request.FormValue("id")
 	name := request.FormValue("name")
-	title := request.FormValue("title")
+	if name == "" {
+		name = "名無しさん"
+	}
 	comment := request.FormValue("comment")
-	wdays := [...]string{"日","月","火","水","木","金","土"}
+	wdays := [...]string{"日", "月", "火", "水", "木", "金", "土"}
 	t := time.Now()
 	date := t.Format("2006-01-02 15:04:05") + " (" + wdays[t.Weekday()] + ")"
 	datfile := "dat/" + id
-	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
-	file, err := os.OpenFile(datfile, options, os.FileMode(0600))
-	check(err)
-	defer file.Close()
-	fmt.Fprintf(file, "%s,%s,%s\n", name, date, comment)
 
-	http.Redirect(writer, request, "/view?id="+id+"&title="+title, http.StatusFound)
+	// read dat and add comment
+	raw, err := ioutil.ReadFile(datfile)
+	check(err)
+	var thread Thread
+	json.Unmarshal(raw, &thread)
+	thread.Comments = append(thread.Comments, Comment{Name: name, Date: date, Cont: comment})
+
+	// write back to dat file
+	dat, err := json.Marshal(thread)
+	check(err)
+	err = ioutil.WriteFile("dat/"+id, dat, 0644)
+	check(err)
+
+	http.Redirect(writer, request, "/view?id="+id, http.StatusFound)
 }
 
 func main() {
