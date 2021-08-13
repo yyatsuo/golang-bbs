71b803af10431f441e91dead87acf88aca5eab2b
Yusuke Yatsuo <yyatsuo@gmail.com>
2021-08-13 21:56:07 +0900

    [add] Support envvar
    Now golang-bbs reads following evn vars!
    * GOLANG_BBS_HOST
    * GOLANG_BBS_PORT
    * GOLANG_BBS_WORDIR

--- bbs.go
+++ bbs.go
@@ -15,6 +15,10 @@ import (
 	"time"
 )
 
+var workdir string
+var host string
+var port string
+
 type Comment struct {
 	Name string `json:"Name"`
 	Date string `json:"Date"`
@@ -35,12 +39,12 @@ func check(err error) {
 }
 
 func threadListHandler(writer http.ResponseWriter, request *http.Request) {
-	_, err := os.Stat("dat")
+	_, err := os.Stat(workdir + "/dat")
 	if err != nil {
-		os.Mkdir("dat", os.FileMode(0755))
+		os.Mkdir(workdir+"/dat", os.FileMode(0755))
 	}
 
-	file, err := os.OpenFile("dat/threads.csv", os.O_CREATE, os.FileMode(0600))
+	file, err := os.OpenFile(workdir+"/dat/threads.csv", os.O_CREATE, os.FileMode(0600))
 	check(err)
 	defer file.Close()
 	reader := csv.NewReader(file)
@@ -54,7 +58,7 @@ func threadListHandler(writer http.ResponseWriter, request *http.Request) {
 		check(err)
 		threads = append(threads, Thread{Title: record[0], Id: record[1]})
 	}
-	html, err := template.ParseFiles("template/top.html")
+	html, err := template.New("top.html").ParseFiles(workdir + "/template/top.html")
 	check(err)
 	err = html.Execute(writer, threads)
 	check(err)
@@ -68,10 +72,10 @@ func threadCreateHandler(writer http.ResponseWriter, request *http.Request) {
 
 	dat, err := json.Marshal(Thread{Title: title, Id: id})
 	check(err)
-	err = ioutil.WriteFile("dat/"+id, dat, 0644)
+	err = ioutil.WriteFile(workdir+"/dat/"+id, dat, 0644)
 	check(err)
 
-	threadfile, err := os.OpenFile("dat/threads.csv", options, os.FileMode(0600))
+	threadfile, err := os.OpenFile(workdir+"/dat/threads.csv", options, os.FileMode(0600))
 	check(err)
 	defer threadfile.Close()
 	fmt.Fprintf(threadfile, "%s,%s\n", title, id)
@@ -82,7 +86,7 @@ func threadCreateHandler(writer http.ResponseWriter, request *http.Request) {
 func threadViewHandler(writer http.ResponseWriter, request *http.Request) {
 
 	id := request.URL.Query().Get("id")
-	datfile := "dat/" + id
+	datfile := workdir + "/dat/" + id
 	_, err := os.Stat(datfile)
 	check(err)
 
@@ -93,10 +97,10 @@ func threadViewHandler(writer http.ResponseWriter, request *http.Request) {
 
 	funcMap := template.FuncMap{
 		"crlf2br": func(s string) template.HTML {
-			return template.HTML(strings.Replace(s, "\r\n", "<br>", -1)) 
+			return template.HTML(strings.Replace(s, "\r\n", "<br>", -1))
 		},
 	}
-	html, err := template.New("view.html").Funcs(funcMap).ParseFiles("template/view.html")
+	html, err := template.New("view.html").Funcs(funcMap).ParseFiles(workdir + "/template/view.html")
 	check(err)
 	check(html.Execute(writer, thread))
 }
@@ -111,7 +115,7 @@ func addCommentHandler(writer http.ResponseWriter, request *http.Request) {
 	wdays := [...]string{"日", "月", "火", "水", "木", "金", "土"}
 	t := time.Now()
 	date := t.Format("2006-01-02 15:04:05") + " (" + wdays[t.Weekday()] + ")"
-	datfile := "dat/" + id
+	datfile := workdir + "/dat/" + id
 
 	// read dat and add comment
 	raw, err := ioutil.ReadFile(datfile)
@@ -123,17 +127,33 @@ func addCommentHandler(writer http.ResponseWriter, request *http.Request) {
 	// write back to dat file
 	dat, err := json.Marshal(thread)
 	check(err)
-	err = ioutil.WriteFile("dat/"+id, dat, 0644)
+	err = ioutil.WriteFile(datfile, dat, 0644)
 	check(err)
 
 	http.Redirect(writer, request, "/view?id="+id, http.StatusFound)
 }
 
 func main() {
+	workdir = os.Getenv("GOLANG_BBS_WORKDIR")
+	if workdir == "" {
+		workdir, _ = os.Getwd()
+	}
+	host = os.Getenv("GOLANG_BBS_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port = os.Getenv("GOLANG_BBS_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Println("Start Server")
+	fmt.Println("Host     ", host, port)
+	fmt.Println("Workdir  ", workdir)
+
 	http.HandleFunc("/", threadListHandler)
 	http.HandleFunc("/create", threadCreateHandler)
 	http.HandleFunc("/view", threadViewHandler)
 	http.HandleFunc("/comment", addCommentHandler)
-	err := http.ListenAndServe("localhost:8080", nil)
+	err := http.ListenAndServe(host+":"+port, nil)
 	log.Fatal(err)
 }
